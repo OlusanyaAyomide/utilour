c4656ba97a76668966b1224de25a9032b38072f2
OLUSANYA AYOMIDE <ayomideflex72@gmail.com>
2023-10-28 16:17:18 +0100

    'imag-bug'

--- components/landing/home/Hero.tsx
+++ components/landing/home/Hero.tsx
@@ -22,16 +22,16 @@ export default function Hero() {
       </div>
       <div className="lg:pl-2 w-full animate-custombounce lg:w-6/12 mt-8 xs:mt-12 lg:mt-0">
         <div className="max-w-[265px] animate-popinDelay xs:max-w-[300px] sm:max-w-[440px] mx-auto  relative h-[412px] z-20">
-          <Image src={'/Happy.jpg'} alt='happy' fill className='object-contain relative z-20 rounded-2xl overflow-hidden sm:border-[4px] border-white'/>
+          <Image src={'/Happy.JPG'} alt='happy' fill className='object-contain relative z-20 rounded-2xl overflow-hidden sm:border-[4px] border-white'/>
           <div className="absolute z-10 h-[300px] -bottom-20 w-[120%] -left-[10%] bg-white/80 rounded-md "></div>
           <div className='absolute max-xs:top-24 animate-fadeUpDelay1 -left-4 sm:-left-10 top-16  w-[100px] sm:w-[150px] z-20 h-[50px]'>
-          <Image src={'/util.png'} alt='utilze-funds' fill className='object-contain rounded-md'/>
+          <Image src={'/util.PNG'} alt='utilze-funds' fill className='object-contain rounded-md'/>
         </div>
         <div className='absolute animate-fadeUpDelay2 -left-2 sm:-left-10 -bottom-10 sm:-bottom-20  w-[140px] sm:w-[200px] z-20 h-[300px]'>
-          <Image src={'/usave.png'} alt='utilze-funds' fill className='object-contain rounded-md'/>
+          <Image src={'/usave.PNG'} alt='utilze-funds' fill className='object-contain rounded-md'/>
         </div>
         <div className='absolute animate-fadeUpDelay3 right-0 bottom-0 sm:right-2 sm:bottom-2 w-[100px] sm:w-[140px] z-20 h-[160px]'>
-          <Image src={'/products.png'} alt='utilze-funds' fill className='object-contain rounded-md'/>
+          <Image src={'/Products.PNG'} alt='utilze-funds' fill className='object-contain rounded-md'/>
         </div>
         </div>
 
