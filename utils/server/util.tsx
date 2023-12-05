69eeedd3d726856f3ddb9468c0a04194bf1dea3d
OLUSANYA AYOMIDE <ayomideflex72@gmail.com>
2023-12-05 16:24:04 +0100

    'auth-option-2'

--- utils/server/util.tsx
+++ utils/server/util.tsx
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 import {getServerSession} from "next-auth"
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "./authOptions";
 import { redirect } from "next/navigation";
 import { ISessionInterface } from "@/interfaces/interface";
 
