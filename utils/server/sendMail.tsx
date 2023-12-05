bd946d47f8a48f28d50d32271a6f435af63a0c97
OLUSANYA AYOMIDE <ayomideflex72@gmail.com>
2023-12-05 16:09:34 +0100

    'email-server-change'

--- utils/server/sendMail.tsx
+++ utils/server/sendMail.tsx
@@ -1,5 +1,5 @@
 import nodemailer from "nodemailer"
-import axios from "axios";
+// import axios from "axios";
 
 
 // const transporter = nodemailer.createTransport({
@@ -25,40 +25,74 @@ import axios from "axios";
     //     console.log(err)
     // }
 export const mailSender = async ({to,body,subject,name}:{to:string,body:string,subject:string,name:string})=>{
-    const url = "https://api.brevo.com/v3/smtp/email"
-    const requestBody = {
-        sender:{
-            "name":"Utilor Support",
-            "email":"gracesegzy@gmail.com"
-        },
-        to:[
-           {
-               "email":to,
-               "name":name
-           } 
-        ],
-        subject:subject,
-        htmlContent:`<div>
-            <h3>Hi ${name} Welocome to utilor</h3>
-            <h1>${body}</h1>
-            <h4>Use the code below to continue your sign up</h4>
-        </div> `
-    }
-    const headers = {
-          'api-key': process.env.BREVO_KEY as string,
-          'Content-type': 'application/json',
+    console.log("mail triggered")
+    // const url = "https://api.brevo.com/v3/smtp/email"
+    // const requestBody = {
+    //     sender:{
+    //         "name":"Utilor Support",
+    //         "email":"gracesegzy@gmail.com"
+    //     },
+    //     to:[
+    //        {
+    //            "email":to,
+    //            "name":name
+    //        } 
+    //     ],
+    //     subject:subject,
+    //     htmlContent:`<div>
+    //         <h3>Hi ${name} Welocome to utilor</h3>
+    //         <h1>${body}</h1>
+    //         <h4>Use the code below to continue your sign up</h4>
+    //     </div> `
+    // }
+    // const headers = {
+    //       'api-key': process.env.BREVO_KEY as string,
+    //       'Content-type': 'application/json',
           
-    };
-    console.log(process.env.BREVO_KEY)
-    const res = await fetch(url, {
-          method: 'POST',
-          headers: headers,
-          body: JSON.stringify(requestBody),
-         })
-    const result = await res.json()
-    console.log(result)
+    // };
+    // console.log(process.env.BREVO_KEY)
+    // const res = await fetch(url, {
+    //       method: 'POST',
+    //       headers: headers,
+    //       body: JSON.stringify(requestBody),
+    //      })
+    // const result = await res.json()
+    // console.log(result)
     // return result
 
     // const res = await axios.post(url,requestBody,{headers})
     // console.log(res.status)
+    const transporter = nodemailer.createTransport({ 
+    host: process.env.S_EMAIL_HOST as string,
+    port:587,
+    secure: false,
+    auth:{
+        user:process.env.S_EMAIL_USER,
+        pass:process.env.S_EMAIL_PASS
+    }
+    })
+
+    const mailOptions = {
+        from: "ayomideflex72@gmail.com",
+        to,
+        subject,
+        html: `<div>
+                    <h3>Hi ${name} Welocome to utilor</h3>
+                    <br/>
+                    <br/>
+                    <h1>${body}</h1>
+                    <h4>Use the code below to continue your sign up</h4>
+                </div> `,
+    };
+
+    try {
+        const val =await transporter.sendMail(mailOptions);
+        console.log(val.response)
+        return;
+    } catch (error) {
+        console.log(error);
+    }
 }
+
+//S_EMAIL_USER = lekandar11@gmail.com
+//S_EMAIL_PASS = VzHcI0JDGx1RTKSq
